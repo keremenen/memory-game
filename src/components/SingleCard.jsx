e1c9651dd95dcbfd0e5bfb117c4cb577ba5a808d
Kemer <kitowski.przemek@gmail.com>
2023-05-07 15:17:40 +0200

    Added fllipping cards functionality

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -2,7 +2,7 @@ import './SingleCard.css'
 import PropTypes from 'prop-types'
 
 
-export const SingleCard = ({card, handleChoice}) => {
+export const SingleCard = ({card, handleChoice, flipped}) => {
 
     const handleClick = () => {
         handleChoice(card)
@@ -10,7 +10,7 @@ export const SingleCard = ({card, handleChoice}) => {
 
     return(
         <div className='card'>
-            <div>
+            <div className={flipped ? 'flipped' : ''}>
               <img className='front' src={card.src} alt='card-front' />
               <img className='back' src='/img/cover.png' onClick={handleClick} alt='card-back' />
             </div>
@@ -20,7 +20,9 @@ export const SingleCard = ({card, handleChoice}) => {
 }
 
 SingleCard.propTypes = {
-    card: PropTypes.object
+    card: PropTypes.object,
+    handleChoice: PropTypes.func,
+    flipped: PropTypes.bool
 }
 
 export default SingleCard
